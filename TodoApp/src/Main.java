abbfe9376ceb494b0d87f33af0fdb265f68d30c1
Radka Lackova <radkalackova39@gmail.com>
2021-12-11 13:18:57 +0100

    Updated formating

--- TodoApp/src/Main.java
+++ TodoApp/src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Command Line Todo application");
+        System.out.println("=============================");
+        System.out.println();
         System.out.println("Command line arguments:");
         for (String arg : args) {
-            System.out.println(arg);
+            System.out.println("\t" + arg);
         }
+
+
+
+
     }
 }
