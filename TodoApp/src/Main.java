52900d64109f1da1db33326cf0d0aa195566d93b
Radka Lackova <radkalackova39@gmail.com>
2021-12-11 11:25:37 +0100

    Added Argument List

---
+++ TodoApp/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Command line arguments:");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
